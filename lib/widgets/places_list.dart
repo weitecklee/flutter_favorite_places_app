0905b1bfd81c00d3a5f471ce0c432dcd458d3bfb
arkteck <arkteck@gmail.com>
2024-07-16 23:49:57 -0400

    place detail screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places_app/models/place.dart';
+import 'package:favorite_places_app/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -32,6 +33,13 @@ class PlacesList extends StatelessWidget {
               .titleMedium!
               .copyWith(color: Theme.of(context).colorScheme.onSurface),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[i]),
+            ),
+          );
+        },
       ),
     );
   }
